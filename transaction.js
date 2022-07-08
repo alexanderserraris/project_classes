3a69caf05fb1118c94d653cd101f7c71ab819bef
Alexander S <alexanderserraris@git.com>
2022-07-08 16:07:54 +0200

    ex2 done

--- transaction.js
+++ transaction.js
@@ -1,6 +1,25 @@
 class Transaction {
-    constructor(amount, counterParty) {
-        this.amount = amount;
-        this.counterParty = counterParty;
+    constructor(amount, counterparty) {
+        this.amount = parseInt(amount, 10);
+        this.counterparty = counterparty;
+    }
+
+    toString() {
+        return `Transactie(bedrag: ${this.amount} €, tegenpartij: ${this.counterparty})`;
     }
 }
+
+let transactions = [];
+
+function createTransaction() {
+    let amount = document.getElementById("amount").value;
+    let counterparty = document.getElementById("counterparty").value;
+
+    let transaction = new Transaction(amount, counterparty);
+    console.log(`Nieuwe ${transaction.toString()}`);
+
+    transactions.push(transaction);
+    let transactionStrList = transactions.map(t => t.toString());
+    console.log("ALLE TRANSACTIES", transactionStrList);
+}
+
