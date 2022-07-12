49b35dd6761596ae0a7d45956392d1f624d7f48a
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-07-12 10:45:31 +0200

    transaction

--- transaction.js
+++ transaction.js
@@ -1,11 +1,17 @@
 class Transaction {
-    constructor(amount, counterParty) {
+    constructor(amount, sender, receiver) {
         this.amount = amount;
-        this.counterParty = counterParty;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    print() {
+        console.log(`${this.sender} sent ${this.amount} bits to ${this.receiver}`);
     }
 }
 
-let transaction = new Transaction(50, 'John Doe');
-let transactionStr = JSON.stringify(transaction);
+let t1 = new Transaction(50, 'John Doe', 'Jane Doe');
+let t2 = new Transaction(100, 'Mohad Hansen', 'Kamal Khan');
 
-console.log(transactionStr);
+t1.print();
+t2.print();
