a742a72ec08d525584ca8ee5d0af4a5604a108fe
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-07-13 14:03:44 +0200

    way better binding

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-import makeTransactionCard from "./templates/transaction-card.js";
+// import makeTransactionCard from "./templates/transaction-card.js";
+
+function bindData(html, data) {
+    for (const [key, value] of Object.entries(data)) {
+        let element = html.querySelector(`[data-bind="${key}"]`);
+        if (element) {
+            element.innerText = value;
+        }
+    }
+    return html;
+}
+
 
 function showTransactions() {
     let transactions = JSON.parse(localStorage.getItem('transactions'));
 
     let transactionList = document.getElementById('transaction-list');
-    let child = transactionList.lastElementChild;
-    while (child) { // remove children of the current list
-        transactionList.removeChild(child);
-        child = transactionList.lastElementChild;
-    }
 
+    let card = document.querySelector('#transaction-card');
     for (let transaction of transactions) {
-        let transactionCard = makeTransactionCard(transaction);
-        transactionList.appendChild(transactionCard);
+        let clone = card.content.cloneNode(true);
+        let cloneWithData = bindData(clone, transaction);
+        transactionList.appendChild(cloneWithData);
     }
 }
 
